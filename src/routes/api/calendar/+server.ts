1c2c383b038f30a6febed815d3e02095d6f0dc92
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2023-09-21 10:44:44 -0400

    Added in caching of google calendar

--- src/routes/api/calendar/+server.ts
+++ src/routes/api/calendar/+server.ts
@@ -4,7 +4,40 @@ import type { RequestHandler } from './$types';
 import dayjs from 'dayjs';
 import type { CalendarEvent } from '$lib/types';
 
+let cached: CalendarEvent[] = [];
+let lastCached = 0;
+let fetchInProcess = false;
+
 export const GET: RequestHandler = async () => {
+    const cacheTime = Number.parseInt(env.GOOGLE_CALENDAR_CACHE_TIME);
+    if (!Number.isInteger(cacheTime)) throw new Error('Invalid cache time');
+
+    if (fetchInProcess) {
+        console.log('Waiting for calendar events to be fetched...');
+        while (fetchInProcess) {
+            await new Promise(resolve => setTimeout(resolve, 200));
+        }
+
+        console.log('Using cached calendar events');
+        return new Response(JSON.stringify(cached), {
+            headers: {
+                'content-type': 'application/json;charset=UTF-8',
+            },
+        });
+    }
+
+    if (dayjs().valueOf() - lastCached < cacheTime) {
+        console.log('Using cached calendar events');
+        return new Response(JSON.stringify(cached), {
+            headers: {
+                'content-type': 'application/json;charset=UTF-8',
+            },
+        });
+    }
+
+    console.log('Fetching calendar events...');
+    fetchInProcess = true;
+
     const auth = new google.auth.GoogleAuth({
         keyFile: env.GOOGLE_KEY_FILE,
         scopes: ['https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/calendar.readonly'],
@@ -20,6 +53,9 @@ export const GET: RequestHandler = async () => {
     });
 
     const data = res.data.items as CalendarEvent[];
+    cached = data;
+    lastCached = dayjs().valueOf();
+    fetchInProcess = false;
 
     return new Response(JSON.stringify(data), {
         headers: {
